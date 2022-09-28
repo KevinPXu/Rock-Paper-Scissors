66ef7590aa197ebf4ef2f00d3c4b3885b8de2c85
KevinPXu <kevinpxu21@gmail.com>
2022-09-28 14:43:37 -0500

    created recursive playing if player says yes to play again and added win, lose, and tie counter.

--- script.js
+++ script.js
@@ -1,50 +1,78 @@
-var userInput = prompt("Choose R, P or S");
+var playAgain = true;
+var outcome = 0;
+var wins = 0;
+var loses = 0;
+var ties = 0;
 
-while (
-  userInput !== "R" &&
-  userInput !== "P" &&
-  userInput !== "S" &&
-  userInput !== "r" &&
-  userInput !== "p" &&
-  userInput !== "s"
-) {
-  userInput = prompt("That is not a valid entry, please try again");
+while (playAgain == true) {
+  var userInput = prompt("Choose R, P or S");
+  while (
+    userInput !== "R" &&
+    userInput !== "P" &&
+    userInput !== "S" &&
+    userInput !== "r" &&
+    userInput !== "p" &&
+    userInput !== "s"
+  ) {
+    userInput = prompt("That is not a valid entry, please try again");
+  }
+  console.log(userInput);
+
+  userInput = userInput.toUpperCase();
+
+  var choices = ["R", "P", "S"];
+  var arrNum = Math.floor(Math.random() * choices.length);
+  var compInput = choices[arrNum];
+
+  alert("The computer chose " + compInput);
+
+  outcome = gameMech();
+
+  if (outcome === 0) {
+    ties++;
+    alert(
+      "You tied!\n" + "wins: " + wins + "\nloses: " + loses + "\nties: " + ties
+    );
+  } else if (outcome === 1) {
+    wins++;
+    alert(
+      "You Won!\n" + "wins: " + wins + "\nloses: " + loses + "\nties: " + ties
+    );
+  } else {
+    loses++;
+    alert(
+      "You Lost!\n" + "wins: " + wins + "\nloses: " + loses + "\nties: " + ties
+    );
+  }
+
+  playAgain = confirm("Would you like to play again?");
 }
-console.log(userInput);
 
-var choices = ["R", "P","S"];
-var arrNum = Math.floor(Math.random() * choices.length);
-var compInput = choices[arrNum];
-
-alert("The computer chose " + compInput);
-
-function gameMech(){
+function gameMech() {
   var gameOutcome = 0;
-  if (userInput == compInput){
+  if (userInput == compInput) {
     gameOutcome = 0;
     return gameOutcome;
   }
-  if (userInput == "R" && compInput == "P"){
+  if (userInput == "R" && compInput == "P") {
     gameOutcome = -1;
     return gameOutcome;
-  }
-  else if (userInput == "P" && compInput == "R"){
+  } else if (userInput == "P" && compInput == "R") {
     gameOutcome = 1;
     return gameOutcome;
-  }
-  else if (userInput == "P" && compInput == "S"){
+  } else if (userInput == "P" && compInput == "S") {
     gameOutcome = -1;
     return gameOutcome;
-  }
-  else if (userInput == "S" && compInput == "P"){
+  } else if (userInput == "S" && compInput == "P") {
     gameOutcome = 1;
     return gameOutcome;
-  } else if (userInput == "R" && compInput == "S"){
+  } else if (userInput == "R" && compInput == "S") {
     gameOutcome = 1;
     return gameOutcome;
-  }
-  else {
+  } else {
     gameOutcome = -1;
     return gameOutcome;
   }
 }
+
+console.log("game is over");
