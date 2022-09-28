b132fcdf70a8b44c613693628861ed7840d5942b
KevinPXu <kevinpxu21@gmail.com>
2022-09-28 14:00:19 -0500

    added error if user inputs incorrect character to entry

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
-//console logs the global object
-console.log(this);
+var userInput = prompt("Choose R, P or S");
 
-function helloThis() {
-  // this is still referring to the global object even within the function
-  console.log("Inside this function, this is " + this);
+while (
+  userInput !== "R" &&
+  userInput !== "P" &&
+  userInput !== "S" &&
+  userInput !== "r" &&
+  userInput !== "p" &&
+  userInput !== "s"
+) {
+  userInput = prompt("That is not a valid entry, please try again");
 }
-helloThis();
-
-var child = {
-  age: 10,
-  ageTenYears: function () {
-    //console logs 10 + 10 or 20, calls self in object child
-    console.log(this.age + 10);
-  },
-};
-
-var investor = {
-  name: "Cash Saver",
-  investment: {
-    initialInvestment: 5000,
-    investmentGrowth: function () {
-      //console logs the initialInvestment or 5000 from the object this is in.
-      console.log(this.initialInvestment);
-    },
-  },
-};
-
-investor.investment.investmentGrowth();
+console.log(userInput);
